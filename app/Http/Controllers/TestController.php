463913609237dc6fd1aa5a8d702bd3544a671e5a
Bui Duc Thien <thienbd@vmms.com.vn>
2023-03-29 09:58:48 +0700

    try

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,14 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use ModelsPackage\Laravel\Models\Product;
+use ModelsPackage\Laravel\Models\ProductVariant;
 
 class TestController extends Controller
 {
     //
     public function __invoke()
     {
-        $product = Product::find(1)->load('variants');
-        dd($product);
+        $product = Product::with(['variants', 'variants.variantAttributeValues'])->find(1);
+        dd($product->variants);
+
+        // $variants = ProductVariant::find(1);
+        // dd($variants->load('variantAttributeValues'));
         return view('welcome');
     }
 }
