7487da230a511f8796556ff5cca5a283ae62f527
Bui Duc Thien <thienbd@vmms.com.vn>
2023-04-13 22:00:20 +0700

    feat: login google

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -11,11 +11,6 @@ class TestController extends Controller
     //
     public function __invoke()
     {
-        $product = Product::with(['variants', 'variants.variantAttributeValues'])->find(1);
-        dd($product->variants);
-
-        // $variants = ProductVariant::find(1);
-        // dd($variants->load('variantAttributeValues'));
         return view('welcome');
     }
 }
